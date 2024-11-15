ff0e69c1ef5e42d4a9a7dcc872434a47c541e9f8
Louis Bruneteau <bruneteau.louis@gmail.com>
2024-11-15 11:09:04 +0100

    showtime cleaned

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request, jsonify, make_response, send_from_directory
 import json
 from werkzeug.exceptions import NotFound
 
@@ -9,6 +9,15 @@ HOST = '0.0.0.0'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
    schedule = json.load(jsf)["schedule"]
+
+# Swagger documentation
+@app.route("/docs", methods=['GET'])
+def docs():
+	return render_template('swagger_template.html')
+
+@app.route('/spec', methods=['GET'])
+def get_spec():
+	return send_from_directory('.', 'UE-archi-distribuees-Showtime-1.0.0-resolved.yaml')
 
 @app.route("/", methods=['GET'])
 def home():
@@ -22,10 +31,10 @@ def showtimes():
 @app.route("/showmovies/<date>", methods=['GET'])
 def showmovies(date):
    for s in schedule:
-        if str(s['date']) == str(date):
-            res = make_response(jsonify(s), 200)
-            return res
-   return make_response(jsonify({'error': 'showtime date not found'}), 400)
+      if str(s['date']) == str(date):
+         res = make_response(jsonify(s), 200)
+         return res
+   return make_response(jsonify({'error': 'showtime date not found'}), 404)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
